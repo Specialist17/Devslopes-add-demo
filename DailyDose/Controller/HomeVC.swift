716a47b0a8be44bd73dafad79999e86939edf8a7
Specialist17 <arocho.fernando.13@gmail.com>
2018-04-14 22:10:25 -0500

    added test add

--- DailyDose/Controller/HomeVC.swift
+++ DailyDose/Controller/HomeVC.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
+    @IBOutlet weak var bannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let request = GADRequest()
+        request.testDevices = [ "ef04375007b786a6c527b30172df7317" ]
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(request)
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ DailyDose/Services/PurchaseManager.swift
@@ -0,0 +1,14 @@
+//
+//  PurchaseManager.swift
+//  DailyDose
+//
+//  Created by Fernando on 4/14/18.
+//  Copyright © 2018 Specialist. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+class PurchaseManager: NSObject{
+    static let instance = PurchaseManager()
+}
